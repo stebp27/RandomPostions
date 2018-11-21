23a1d6e2203ae6adf25c68c6c7ee7b4d48c71705
stebp27 <45163708+stebp27@users.noreply.github.com>
2018-11-21 09:21:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HelloCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = {"Luca", "Stefano", "Fabio", "Federico", "Alessio", "Andrea",
+            "Gianluca", "Nicoletta", "Francesco", "Valerio", "Matteo", "Cristian"};
+
+             PrintStudents(students);
+        }
+
+        static void PrintStudents(string[] students)
+        {
+            string[] rndOrder = new string[] {"","","","","","","","","","","",""};
+
+            int insert = 0;
+
+            while(insert < 12)
+            {
+
+            Random rnd = new Random();
+            
+            int r = rnd.Next(12);
+            
+                if(rndOrder[r] == "")
+                {
+                rndOrder[r] = students[insert];
+                insert++;
+                }
+           
+            }
+            for(int i=0; i<rndOrder.Length -1; i++)
+            {
+                Console.WriteLine(rndOrder[i++].PadRight(15)+rndOrder[i]);
+            }
+        }
+    }
+}
